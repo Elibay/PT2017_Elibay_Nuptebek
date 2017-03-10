e3e79b112222e6306f5b056cc84b00e9d5bf291c
Elibay <elibay98@gmail.com>
2017-03-10 11:30:02 +0600

    GG

---
+++ Polygon/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace ppppp
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string s = @"C:\Users\Данара\Documents\test";
+			DirectoryInfo dir = new DirectoryInfo(s);
+			string ss = String.Empty;
+			StreamWriter sw = new StreamWriter("result.txt");
+			FileInfo[] f = dir.GetFiles();
+			foreach (FileInfo fs in f)
+			{
+				StreamReader sr = new StreamReader(fs.FullName);
+				if (sr.ReadLine() == "yes")
+				{
+					ss = ss + fs.Name;
+					ss = ss + "\n";
+				}
+				sw.WriteLine(ss);
+				sw.Close();
+				sr.Close();
+			}
+		}
+	}
+}
